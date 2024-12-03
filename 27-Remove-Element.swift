71b301a297f7c1b450ef35bdc2bead464e94edd8
River <54134632+hrjin0308@users.noreply.github.com>
2024-12-03 10:28:17 +0900

    [27] [Time Beats: 100.00%] [Memory Beats: 65.85%] - LeetPush

--- 27-Remove-Element.swift
+++ 27-Remove-Element.swift
@@ -1,17 +1,6 @@
 class Solution {
     func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
-        var count = 0
-        var i = 0
-        
-        while i < (nums.count - count) {
-            if nums[i] == val {
-                nums.swapAt(i, (nums.count - count - 1))
-                count += 1
-            } else {
-                i += 1
-            }
-        }
-        
-        return nums.count - count
+        nums = nums.filter({ $0 != val })
+        return nums.count
     }
 }
